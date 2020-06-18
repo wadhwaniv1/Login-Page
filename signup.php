0edcee4da923d6fc98ee546b76d8b8f483855f54
Vishal Wadhwani <62770603+wadhwaniv1@users.noreply.github.com>
2020-06-19 02:42:31 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -30,8 +30,8 @@
 				
 			
 				
-		  echo "<h1>You have successfully logged in...Going to Technoguruamit.com...</h1>"; 
-		  header( "refresh:2; url=http://www.technoguruamit.com" );}	
+		  echo "<h1>You have successfully logged in...Going to website...</h1>"; 
+		  header( "refresh:2; url=http://www.gogle.com" );}	
 			else {
 				echo "<h1>Password mismatched..Please enter the correct password</h1>";
 			}
